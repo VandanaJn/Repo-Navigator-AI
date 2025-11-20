79c4475c41f47a7ec183b4cab942a682a76a0657
Vandana Jain <vandanaj@hotmail.com>
2025-11-20 10:40:34 -0500

    Uncommenting context compaction and session service. Adding first eval. (vj)

--- agents/repo_navigator/agent.py
+++ agents/repo_navigator/agent.py
@@ -3,7 +3,7 @@ from repo_navigator.sub_agents.architecture_agent import architecture_summarizer
 from google.adk.sessions import InMemorySessionService
 from google.adk.runners import Runner
 from google.adk.apps.app import App, EventsCompactionConfig
-from repo_navigator.tools.githubtools import get_repo_structure
+
 
 INSTRUCTION_ROOT = """You are a versatile repository analysis expert. When a user asks about the architecture 
 or structure of a codebase, use the 'Code_Architecture_Agent' tool to get a detailed answer. 
@@ -22,19 +22,19 @@ root_agent = LlmAgent(
     sub_agents=[architecture_summarizer_agent],
 )
 
-# root_app_compacting = App(
-#     name="repo_analysis_app_compacting",
-#     root_agent=root_agent,
-#     events_compaction_config=EventsCompactionConfig(
-#         compaction_interval=3,  # Trigger compaction every 3 invocations
-#         overlap_size=1,  # Keep 1 previous turn for context
-#     ),
-# )
+root_app_compacting = App(
+    name="repo_analysis_app_compacting",
+    root_agent=root_agent,
+    events_compaction_config=EventsCompactionConfig(
+        compaction_interval=3,  # Trigger compaction every 3 invocations
+        overlap_size=1,  # Keep 1 previous turn for context
+    ),
+)
 
-# session_service = InMemorySessionService() 
-# runner = Runner(
-#     app=root_app_compacting,
-#     session_service=session_service 
-# )
+session_service = InMemorySessionService() 
+runner = Runner(
+    app=root_app_compacting,
+    session_service=session_service 
+)
 
 
